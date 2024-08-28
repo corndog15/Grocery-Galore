541126f8ebb954f5ba3e940b0985db6f2e29e9fd
Black Magic <dlyoungworking@gmail.com>
2024-08-28 19:42:04 -0400

    added javaScript for light/dark, switching pages, collecting info into local storage, and creating an empty array

---
+++ JS/script.js
@@ -0,0 +1,37 @@
+const toggle = document.querySelector("#toggle")
+
+// use to switch between pages
+let redirectURL = '';
+
+const redirectPage = function (url) {
+  redirectURL = url;
+  location.assign(url);
+};
+
+// takes a given object and saves the new data to the existing blog data in local storage.
+function storeLocalStorage(groceryList) {
+    localStorage.setItem('groceryList ', JSON.stringify(groceryList))
+  };
+
+
+ // reads from local storage and returns the data. If no data exists, return an empty array.
+function readLocalStorage() {
+    const groceryList = JSON.parse(localStorage.getItem('groceryList'))
+    return groceryList ? groceryList : []
+  };
+
+  // the following code is to toggle between light and dark mode
+      
+if (localStorage.getItem('theme') === 'dark') {
+    document.body.classList.add('dark')
+  }
+  
+  toggle.addEventListener('click', function() {
+    if (document.body.classList.contains('dark')) {
+      document.body.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
+    } else {
+      document.body.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+    }
+  });
